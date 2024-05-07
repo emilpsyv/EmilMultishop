f8679707671bc650b79a74da13c0abf92cd875bb
akula uni <tu7pfrcay@code.edu.az>
2024-05-07 13:09:23 +0400

    first commit

---
+++ MULTISHOP/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MULTISHOP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Contact()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MULTISHOP/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MULTISHOP.Controllers
+{
+    public class ProductController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detail()
+        {
+            return View();
+        }
+    }
+}
